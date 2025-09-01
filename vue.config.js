5753d96cdc348724520fed626e3a707aea5a6ff6
Sunil <sunils.octal8@gmail.com>
2025-09-01 16:40:35 +0530

    live the amaze web

---
+++ vue.config.js
@@ -0,0 +1,13 @@
+const { defineConfig } = require("@vue/cli-service");
+module.exports = defineConfig({
+  transpileDependencies: true,
+  lintOnSave: false,
+  devServer: {
+    host: "0.0.0.0",
+    port: 8081,
+    allowedHosts: "all",
+    client: {
+      overlay: false,
+    },
+  },
+});
